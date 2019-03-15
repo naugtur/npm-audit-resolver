5d449def68c971ea5d5d1e8e49064f96b3504983
Rouan Wilsenach <rouanw@users.noreply.github.com>
2019-03-15 20:06:09 +0200

    Add option for check-audit to output JSON (#10)
    * add option for check-audit to output json
    
    * Include filtered actions, advisories and metadata in json output
    
    to remain consistent with the output of `npm audit --json`
    
    * document --json option
    
    * use Object.keys for node 4 compatibility

--- check.js
+++ check.js
@@ -1,33 +1,56 @@
 #!/usr/bin/env node
 const core = require("./index");
 const npmFacade = require('./src/npmfacade');
+const argv = require('./src/arguments').get();
+
+function auditOk(issues) {
+    return !(issues && issues.length);
+}
+
+function printHumanReadableReport(issues) {
+    if (auditOk(issues)) {
+        console.log("audit ok.");
+        return;
+    }
+    issues.forEach(issue => {
+        console.log(
+            `--------------------------------------------------`
+        );
+        console.log(`[${issue.severity}] ${issue.title}`);
+        console.log(issue.items.map(item => item.report).join("\n"));
+    })
+
+    console.log(
+        `--------------------------------------------------`
+    );
+    console.error(" 😱   Unresolved issues found!");
+    console.log(
+        `--------------------------------------------------`
+    );
+}
+
+function printJsonReport(issues) {
+    console.log(JSON.stringify(issues));
+}
 
 npmFacade.runNpmCommand('audit', { ignoreExit: true })
     .then(input => {
-        console.log(`Total of ${input.actions.length} actions to process`);
+        if (!argv.json) {
+            console.log(`Total of ${input.actions.length} actions to process`);
+        }
         return core.checkAudit(input)
     })
-    .then(issues => {
-        if(issues && issues.length){
-            issues.forEach(issue => {
-                console.log(
-                    `--------------------------------------------------`
-                );
-                console.log(`[${issue.severity}] ${issue.title}`);
-                console.log(issue.items.map(item => item.report).join("\n"));
-            })
-
-            console.log(
-                `--------------------------------------------------`
-            );
-            console.error(" 😱   Unresolved issues found!");
-            console.log(
-                `--------------------------------------------------`
-            );
+    .then(result => {
+        const { issues } = result;
+        if (argv.json) {
+            printJsonReport(result);
+        } else {
+            printHumanReadableReport(issues);
+        }
+        if (!auditOk(issues)) {
             process.exit(1);
         }
-    })
-    .then(() => console.log("audit ok."))
+})
     .catch(e => {
         console.error(e);
         process.exit(2);
