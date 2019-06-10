9a0900ff74c5360cc037483ffdc3bfeabfa5665f
Naugtur <naugtur@gmail.com>
2019-06-10 11:58:15 +0200

    update schema test stub

--- test/auditFile/schemas.js
+++ test/auditFile/schemas.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const assert = require("assert");
-const auditFile = require("../../src/auditFile")
+const auditFile = require("../../src/core/auditFile/fileHandle")
 
 auditFile.load(path.resolve(__dirname,"./v1ok.json"))
 
 assert.throws(()=>{
     auditFile.load(path.resolve(__dirname,"./v1wrong.json"))
 })
+
+console.log("ok")
