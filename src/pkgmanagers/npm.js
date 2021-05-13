61a18b7f89156658f13c3289f1709791bcbb42e3
naugtur <naugtur@gmail.com>
2021-05-13 23:52:38 +0200

    reindexing1

--- src/pkgmanagers/npm.js
+++ src/pkgmanagers/npm.js
@@ -12,6 +12,73 @@ function getCommand(action) {
     }
 }
 
+
+function reformatFromV2(input){
+    const vulns = input.vulnerabilities;
+    const paths = {}
+    const recordPath = (via, fixAvailable, acc) => {
+        if(!paths[acc]){
+            paths[acc]=[];
+        }
+        paths[acc].push({
+            id: via.source,
+            name: via.name,
+            title: via.title,
+            url: via.url,
+            severity: via.severity,
+            range: via.range,
+            fixAvailable,
+            path: acc
+
+        })
+    }
+    const indexPaths = (name, accumulator) => {
+        const vuln = vulns[name]
+        if(accumulator){
+            accumulator+=`>${name}`
+        } else {
+            accumulator = name
+        }
+
+        vuln.via.forEach(via => {
+            if (typeof via === "string") {
+                indexPaths(via, accumulator)
+            } else {
+                recordPath(via, vuln.fixAvailable, accumulator)
+            }
+        })
+    }
+
+    Object.keys(vulns).forEach(name => indexPaths(name))
+    return paths;
+    // .
+    // {
+    // "name": "base64url",
+    //   path: "", //index paths form via 
+    //   id: input..via.source,
+    //   "title": "Out-of-bounds Read",
+    //       "url": "https://npmjs.com/advisories/658",
+    //       "severity": "moderate",
+          
+    //   "range": "<3.0.0",
+      
+    //   "fixAvailable": {
+    //     "name": "base64url", //module
+    //     "version": "3.0.1", //target
+    //     "isSemVerMajor": true //isMajor
+    //   }
+    // }
+}
+
+function reformat(input){
+    switch(input.auditReportVersion){
+        case 2:
+            return reformatFromV2(input)
+        default: 
+            return reformatFromLegacy(input)
+    } 
+}
+
 module.exports = {
     version: 1,
     getAudit({ promiseCommand, argv, shellOptions }) {
@@ -33,6 +100,8 @@ module.exports = {
                 }
                 return parsed
             })
+            .then(reformat)
+            .then(console.log)
         //TODO: retries on ENOAUDIT
     },
     fix({ promiseCommand, argv, shellOptions, action }) {
