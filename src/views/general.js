e262d9696bc4d763c3f84b3b0d14a5cbf1439b4e
naugtur <naugtur@gmail.com>
2023-02-14 20:36:29 +0100

    warn when using interactive resolver in CI

--- src/views/general.js
+++ src/views/general.js
@@ -1,4 +1,7 @@
 const view = {
+    ciDetected() {
+        console.error(`'resolve-audit' is the interactive wizard to make decisions and should not be used in CI. Use 'check-audit' instead. It's a wrapper of 'npm audit' that takes your decisions into account.`)
+    },
     totalActions(length) {
         console.log(`Total of ${length} actions to process`);
 
